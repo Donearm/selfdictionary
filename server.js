ff29ba57190354091dd46a1cfc19cafbabfdd8d8
Gianluca Fiore <forod.g@gmail.com>
2018-12-03 10:55:55 +0100

    add findTranslation function on the db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const assert = require("assert");
 const mongo = require('mongodb');
 
 const MongoClient = require("mongodb").MongoClient;
@@ -11,12 +12,29 @@ const config = require('./config');
 // Establish a mongodb connection using settings from the config.js file
 const dburl = `mongodb://${config.db.host}/${config.db.name}`;
 
-MongoClient.connect(dburl, { useNewUrlParser: true }, function(err, database) {
+MongoClient.connect(dburl, { useNewUrlParser: true }, function(err, client) {
 	if (err) return console.error(err);
 
 	console.log("Connection to DB done");
-	db = database;
+	db = client.db("dictionary");
+	findTranslation(db, "Deutsch", "forderungen", function() {
+		client.close();
+	});
+
 });
+
+// Look up a word in the dictionary db
+// 3 parameters, db = database, coll = collection name, term = the 
+// word we are looking for, cb = callback function
+const findTranslation= function(db, coll, term, cb) {
+	const collection = db.collection(coll);
+	collection.find({'Word': term}).toArray(function(err, docs) {
+		assert.equal(err, null);
+		console.log("Found the following results: ");
+		console.log(docs);
+		cb(docs);
+	});
+}
 
 
 app.listen(8888, function() {
