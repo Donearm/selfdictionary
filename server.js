901304604f2d7b754f58ff4a59c48d1d3c482462
Gianluca Fiore <forod.g@gmail.com>
2018-12-25 17:59:26 +0100

    handling form validation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const validator = require("validator");
 const assert = require("assert");
 const mongo = require('mongodb');
 
@@ -15,7 +16,7 @@ app.listen(8888, function() {
 	console.log("Listening on http://localhost:8888");
 });
 
-app.get('/', function(req, res) {
+app.all('/', function(req, res) {
 	res.sendFile(__dirname + "/index.html")
 });
 
@@ -30,19 +31,27 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post("/lookup", function(req, res) {
-	const term = req.body.term;
-	MongoClient.connect(dburl, { useNewUrlParser: true }, function(err, client) {
-		if (err) return console.error(err);
+	const term = req.body.term; 
+	if (!validator.isLength(term, { min: 2 })) {
+		res.status(400);
+		return res.send("Word to look up should be at least 2 characters long");
+	} else if (!validator.isAlpha(term)) {
+		res.status(400);
+		return res.send("Only alphanumeric characters are accepted");
+	} else {
+		MongoClient.connect(dburl, { useNewUrlParser: true }, function(err, client) {
+			if (err) return console.error(err);
 
-		console.log("Connection to DB done");
-		db = client.db("dictionary");
-		db.collection("Deutsch").find({'Word': term}).toArray(function(err, docs) {
-			assert.equal(err, null);
-			console.log("Found the following results: ");
-			console.log(docs);
-			res.setHeader("Content-Type", "text/plain");
-			res.send(docs);
-			res.end();
+			console.log("Connection to DB done");
+			db = client.db("dictionary");
+			db.collection("Deutsch").find({'Word': term}).toArray(function(err, docs) {
+				assert.equal(err, null);
+				console.log("Found the following results: ");
+				console.log(docs);
+				res.setHeader("Content-Type", "text/plain");
+				res.send(docs);
+				res.end();
+			});
 		});
-	});
+	}
 });
