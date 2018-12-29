ab33e5947fb3f2877b022d4ac9111eb863ef3807
Gianluca Fiore <forod.g@gmail.com>
2018-12-29 12:04:36 +0100

    bail out if no lang button is pressed

--- js/main.js
+++ js/main.js
@@ -14,6 +14,12 @@ $(document).ready(function() {
 function showTranslation(language) {
 	// Get the term in the text form
 	let term = $("#term").serializeArray();
+	if (!language) {
+		$('#content').text("No language set. Please choose the language you're looking to translate from");
+		return false
+	}
+	term[0].lang = language;
+
 
 	$.ajax({
 		url: '/lookup',
